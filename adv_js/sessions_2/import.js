5b5cd2451b01666ccbaadd54a99a233989046018
[YOUNES ADI] <younesadi18@gmail.com>
2022-04-05 05:43:48 +0100

    fullstack_1

--- adv_js/sessions_2/import.js
+++ adv_js/sessions_2/import.js
@@ -9,10 +9,10 @@
 // fs.writeFileSync('./read-me.txt', "\nhello motherfucker",{encoding:'utf-8',flag:'w'})
 // console.log(data);
 
-// const path=require('path');
-// const call=require('./main.js');
-// const funcpath=path.join(__dirname,"css.txt")
-// console.log(funcpath);
-// const relation=path.relative("import.js","C:\Users\Surface\Desktop\MC\Full_stack_training\Home_Works\JS_Home_Work_3.js");
-// console.log(relation);
-// call.call();
+const path=require('path');
+const call=require('./main.js');
+const funcpath=path.join(__dirname,"css.txt")
+console.log(funcpath);
+const relation=path.relative("import.js","C:\Users\Surface\Desktop\MC\Full_stack_training\Home_Works\JS_Home_Work_3.js");
+console.log(relation);
+call.call();
