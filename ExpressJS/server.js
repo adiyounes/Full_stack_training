4620e1c0c745edc56a74f4212d95272962b84355
[YOUNES ADI] <younesadi18@gmail.com>
2022-04-08 11:17:37 +0100

    express_js_exercing

---
+++ ExpressJS/server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+
+app.set('view engine','ejs')
+
+app.get('/',(req, res)=>{
+    console.log('Here');
+    res.render("index",{text:"World"})
+})
+
+const userRouter=require('./Router/users');
+app.use('/users',userRouter)
+app.listen(3000)
