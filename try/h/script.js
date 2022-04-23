6ceaccd0ac2e76c93343bbda21121b286b9a8369
[YOUNES ADI] <younesadi18@gmail.com>
2022-04-23 12:55:30 +0100

    FULL_STACK_PROJECT_1

---
+++ try/h/script.js
@@ -0,0 +1,35 @@
+
+
+
+
+// function slidePlus(cN){
+//     console.log(document.getElementsByClassName(cN)[1].getAttribute("class"));
+
+
+
+//     var array=document.getElementsByClassName(cN);
+//     console.log(array.length);
+    
+//     var temp=document.getElementsByClassName(cN)[0].getAttribute("class");
+//     console.log(temp);
+
+//     for (let i = 0; i < array.length-1; i++){
+//         let temp2=document.getElementsByClassName(cN)[i+1].getAttribute("class");
+//         console.log(temp2);
+//         document.getElementsByClassName(cN)[i].setAttribute("class",temp2);
+//     }
+
+//     document.getElementsByClassName(cN)[array.length-1].setAttribute("class",temp);
+//     console.log(document.getElementsByClassName(cN)[0].getAttribute("class"));
+    
+
+
+
+    // temp1.setAttribute("class",temp2.getAttribute("class")); 
+ 
+    // array[array.length-1].src=array[0].src;
+
+    // for(let i=1;i<array.length-1;i++){
+    //     array[i].src=array[i+1].src;
+    // }
+    // array[array.length-2]=temp;
